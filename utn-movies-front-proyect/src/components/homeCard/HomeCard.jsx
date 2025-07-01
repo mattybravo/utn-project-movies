b787c4ec023e1f03972d545945ecd4c435b91d2b
mattybravo <mb7570810@gmail.com>
2025-07-01 19:04:28 -0300

    proyecto

--- utn-movies-front-proyect/src/components/homeCard/HomeCard.jsx
+++ utn-movies-front-proyect/src/components/homeCard/HomeCard.jsx
@@ -6,11 +6,6 @@ export default function HomeCard({ image, title, id, onReviewClick }) {
             <div className="movie-card">
                 <img src={image} alt={title} className="movie-poster" />
                 <h5 className="text-center mt-2">{title}</h5>
-                <div className="stars d-flex gap-2 star-rating">
-                    {[...Array(5)].map((_, i) => (
-                        <span className="star" key={i}>★</span>
-                    ))}
-                </div>
                 <button className="review-btn" onClick={ () => {console.log(`Boton clickeando, movie Id`, id); onReviewClick(id) }}>Hacer Reseña</button>
             </div>
         </>
